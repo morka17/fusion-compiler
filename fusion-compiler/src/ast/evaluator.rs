94b47604ccf48447f62d5caae34535aebd1ad1a0
morka17 <joshuamorka19@gmail.com>
2023-08-02 23:26:12 +0100

    added some comments

--- fusion-compiler/src/ast/evaluator.rs
+++ fusion-compiler/src/ast/evaluator.rs
@@ -1,31 +1,39 @@
+// Import the ASTVisitor trait to define ASTEvaluator as a visitor for the AST nodes.
 use super::ASTVisitor;
 
-pub struct ASTEvaluator{
+// Define the ASTEvaluator struct to evaluate the AST nodes.
+pub struct ASTEvaluator {
     pub(crate) last_value: Option<i64>,
 }
 
 impl ASTEvaluator {
+    // Create a new ASTEvaluator instance with the last_value set to None.
     pub fn new() -> Self {
-        Self{last_value: None}
+        Self { last_value: None }
     }
 }
 
+// Implement the ASTVisitor trait for the ASTEvaluator struct.
 impl ASTVisitor for ASTEvaluator {
+    // Implement the visit_number method to handle visiting a number node in the AST.
     fn visit_number(&mut self, number: &super::ASTNumberExpression) {
-        self.last_value = Some(number.number)
+        self.last_value = Some(number.number) // Set last_value to the value of the number node.
     }
 
+    // Implement the visit_binary_expression method to handle visiting a binary expression node in the AST.
     fn visit_binary_expression(&mut self, expr: &super::ASTBinaryExpression) {
-        self.visit_expression(&expr.left);
-        let left = self.last_value.unwrap();
-        self.visit_expression(&expr.right);
-        let right = self.last_value.unwrap();
+        self.visit_expression(&expr.left); // Recursively visit the left-hand side of the binary expression.
+        let left = self.last_value.unwrap(); // Get the value of the left-hand side expression.
+
+        self.visit_expression(&expr.right); // Recursively visit the right-hand side of the binary expression.
+        let right = self.last_value.unwrap(); // Get the value of the right-hand side expression.
+
+        // Evaluate the binary expression based on the operator and update last_value with the result.
         self.last_value = Some(match expr.operator.kind {
             super::ASTBinaryOperatorKind::Plus => left + right,
             super::ASTBinaryOperatorKind::Minus => left - right,
             super::ASTBinaryOperatorKind::Multiply => left * right,
             super::ASTBinaryOperatorKind::Divide => left / right,
-        })
-
+        });
     }
 }
--- fusion-compiler/src/ast/parser.rs
+++ fusion-compiler/src/ast/parser.rs
@@ -1,123 +1,125 @@
+// Import necessary modules and types
 use crate::ast::ASTStatement;
 use crate::ast::lexer::{Lexer, Token};
 
 use super::{ASTExpression, ASTBinaryOperator, ASTBinaryOperatorKind};
 use super::lexer::TokenKind;
 
-
+// Define the Parser struct to process tokens
 pub struct Parser {
     tokens: Vec<super::lexer::Token>,
-    current: usize, 
+    current: usize,
 }
 
-
 impl Parser {
-
-    pub fn new(
-        tokens: Vec<Token>
-    ) -> Self {
-        Self {tokens: tokens.iter().filter(|token| token.kind  != TokenKind::Whitespace).map(|token| token.clone()).collect(), current: 0}
+    // Create a new Parser instance from a vector of tokens
+    pub fn new(tokens: Vec<Token>) -> Self {
+        // Remove whitespace tokens and store non-whitespace tokens in 'tokens' field
+        Self {
+            tokens: tokens
+                .iter()
+                .filter(|token| token.kind != TokenKind::Whitespace)
+                .map(|token| token.clone())
+                .collect(),
+            current: 0,
+        }
     }
 
+    // A convenience function to create a new Parser instance from a vector of tokens
     pub fn from_tokens(tokens: Vec<Token>) -> Self {
         Self::new(tokens)
     }
 
-    pub fn next_statement(&mut self) -> Option<ASTStatement>{
+    // Parse the next statement in the token stream
+    pub fn next_statement(&mut self) -> Option<ASTStatement> {
         let token = self.current()?;
+        // If the current token is EOF, return None to signal the end of parsing
         if token.kind == TokenKind::EOF {
             return None;
         }
+        // Otherwise, parse the statement and return the result
         return self.parse_statement();
-
     }
 
-    fn parse_statement(&mut self) -> Option<ASTStatement>{
+    // Parse a statement, which is essentially an expression in this simplified example
+    fn parse_statement(&mut self) -> Option<ASTStatement> {
         let token = self.current()?;
-        let expr = self.parse_expression()?;
-        return Some(ASTStatement::expression(expr))
+        let expr = self.parse_expression()?; // Parse the expression part of the statement
+        return Some(ASTStatement::expression(expr));
     }
 
-    fn parse_expression(&mut self) -> Option<ASTExpression>{
-        self.parse_binary_expression(0)
-    } 
+    // Parse an expression, which may include binary operations
+    fn parse_expression(&mut self) -> Option<ASTExpression> {
+        self.parse_binary_expression(0) // Start with the lowest precedence (0) for binary operators
+    }
 
-    fn parse_binary_expression(&mut self, precedence: u8) -> Option<ASTExpression>{
-        let mut left = self.parse_primary_expression()?;
+    // Parse a binary expression with a given precedence level
+    fn parse_binary_expression(&mut self, precedence: u8) -> Option<ASTExpression> {
+        let mut left = self.parse_primary_expression()?; // Parse the left-hand side of the binary expression
 
+        // Keep parsing binary operators and their right-hand operands until the precedence is lower
         while let Some(operator) = self.parse_binary_operator() {
-            self.consume();
-            let operator_precedence = operator.precedence();
+            self.consume(); // Consume the operator token
+            let operator_precedence = operator.precedence(); // Get the precedence of the operator
             if operator_precedence < precedence {
                 break;
             }
-            let right = self.parse_binary_expression(operator_precedence)?;
-            left = ASTExpression::binary(operator, left, right);
+            let right = self.parse_binary_expression(operator_precedence)?; // Parse the right-hand side
+            left = ASTExpression::binary(operator, left, right); // Create a binary expression node
         }
 
         return Some(left);
-    } 
+    }
 
-    fn parse_binary_operator(&mut self) -> Option<ASTBinaryOperator>{
+    // Parse a binary operator
+    fn parse_binary_operator(&mut self) -> Option<ASTBinaryOperator> {
         let token = self.current()?;
-        let kind =  match token.kind {
-            TokenKind::Plus => {
-                Some(ASTBinaryOperatorKind::Plus)
-            },
-            TokenKind::Minus =>{
-                 Some(ASTBinaryOperatorKind::Minus)
-                },
-            TokenKind::Asterisk => {
-                Some(ASTBinaryOperatorKind::Multiply)
-            },
-            TokenKind::Slash => {
-                Some(ASTBinaryOperatorKind::Divide)
-            },
+        let kind = match token.kind {
+            // Map token kinds to corresponding binary operator kinds
+            TokenKind::Plus => Some(ASTBinaryOperatorKind::Plus),
+            TokenKind::Minus => Some(ASTBinaryOperatorKind::Minus),
+            TokenKind::Asterisk => Some(ASTBinaryOperatorKind::Multiply),
+            TokenKind::Slash => Some(ASTBinaryOperatorKind::Divide),
             _ => None,
         };
 
+        // If the token is a valid binary operator, create and return the corresponding operator node
         return kind.map(|kind| ASTBinaryOperator::new(kind, token.clone()));
     }
 
+    // Parse a primary expression, which can be a number or a parenthesized expression
     fn parse_primary_expression(&mut self) -> Option<ASTExpression> {
-        let token = self.consume()?;
+        let token = self.consume()?; // Consume the current token
         match token.kind {
             TokenKind::Number(number) => {
-                return Some(ASTExpression::number(number));
+                return Some(ASTExpression::number(number)); // Create a number node
             },
-            // TokenKind::Plus => todo!(),
-            // TokenKind::Minus => todo!(),
-            // TokenKind::Asterisk => todo!(),
-            // TokenKind::Slash => todo!(),
             TokenKind::LeftParen => {
-                let expr = self.parse_expression()?;
+                let expr = self.parse_expression()?; // Parse the expression inside the parentheses
                 let token = self.consume()?;
                 if token.kind != TokenKind::RightParen {
                     panic!("Expected right paren");
                 }
-                Some(
-                    ASTExpression::paranthesized(expr)
-                )
+                Some(ASTExpression::paranthesized(expr)) // Create a parentheses expression node
             },
-            // TokenKind::RightParen => todo!(),
-            // TokenKind::Whitespace => todo!(),
-            // TokenKind::EOF => todo!(),
-            // TokenKind::Bad => todo!(),
             _  => {
-                None
-            } 
+                None // Return None for unsupported primary expressions
+            }
         }
     }
 
-    fn peek(&self, offset: isize) -> Option<&Token>{
+    // Peek at a token with a given offset from the current position
+    fn peek(&self, offset: isize) -> Option<&Token> {
         self.tokens.get((self.current as isize + offset) as usize)
     }
 
+    // Get the current token
     fn current(&self) -> Option<&super::lexer::Token> {
         self.peek(0)
     }
 
-    fn consume(&mut self) -> Option<&Token>{
+    // Consume the current token and move to the next one
+    fn consume(&mut self) -> Option<&Token> {
         self.current += 1;
         let token = self.peek(-1)?;
         return Some(token);
